21709bc0f4edb85eb3d821214135040fa2c7f617
Maria Victoria <70769530+M-VictoriaCM@users.noreply.github.com>
2023-04-12 19:21:27 -0300

    Update Main.java

--- Practica2/src/ParteA/Ejercicio1/Main.java
+++ Practica2/src/ParteA/Ejercicio1/Main.java
@@ -5,8 +5,7 @@ public class Main {
         Recursividad re = new Recursividad();
         int[] arreglo = {1, 2, 3, 4, 5};
         int[] arreglo1 = {10, 5, 13, 1, 5};
-
-        System.out.println(re.estaOrdenado(arreglo, arreglo.length-1));
-        System.out.println(re.estaOrdenado(arreglo1, arreglo1.length-1));
+        System.out.println(re.estaOrdenado(arreglo, 0));
+        System.out.println(re.estaOrdenado(arreglo1, 0));
     }
 }
